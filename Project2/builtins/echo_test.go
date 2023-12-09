6491a7fd851e79a77be0681e38d8f825156e1488
Alex Ho <hoalex219@gmail.com>
2023-12-09 04:26:04 -0600

    echo tests

---
+++ Project2/builtins/echo_test.go
@@ -0,0 +1,42 @@
+package builtins_test
+
+import (
+	"testing"
+
+	"github.com/Dicathen/CSCE4600/Project2/builtins"
+)
+
+func TestEcho(t *testing.T) {
+	type args struct {
+		args []string
+	}
+	//build tests
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "no args should print newline",
+		},
+		{
+			name: "* arg should print files in current directory",
+			args: args{args: []string{"*"}},
+		},
+		{
+			name: "args should print args",
+			args: args{args: []string{"abc", "def"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//testing
+			if err := builtins.Echo(tt.args.args...); err != nil {
+				if tt.wantErr == nil {
+					t.Errorf("Echo() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+		})
+	}
+}
