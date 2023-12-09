bd198c0be4db7608b1a386327519b56527dff732
Alex Ho <hoalex219@gmail.com>
2023-12-09 04:13:26 -0600

    push echo, still need tests

---
+++ Project2/builtins/echo.go
@@ -0,0 +1,39 @@
+package builtins
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func Echo(args ...string) error {
+	if len(args) == 0 {
+		return errors.New("echo: expected at least one argument")
+	} else if len(args) == 1 && args[0] == "*" {
+		//print files in current directory
+
+		//get current directory
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		//get files in current directory
+		files, err := ioutil.ReadDir(wd)
+		if err != nil {
+			return err
+		}
+
+		//print files in current directory in a single line
+		for _, file := range files {
+			fmt.Print(file.Name() + " ")
+		}
+		fmt.Println()
+
+		return nil
+	}
+	fmt.Println(strings.Join(args, " "))
+	return nil
+}
--- Project2/main.go
+++ Project2/main.go
@@ -80,6 +80,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return builtins.EnvironmentVariables(w, args...)
 	case "pwd":
 		return builtins.PrintWorkingDirectory(args...)
+	case "echo":
+		return builtins.Echo(args...)
 	case "exit":
 		exit <- struct{}{}
 		return nil
